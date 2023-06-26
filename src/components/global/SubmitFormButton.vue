c83314f5ec1b60ed6dda60e740ba77a4dcf04d62
shaikram <shaikram02@gmail.com>
2023-06-25 23:22:36 -0700

    Resend Email

--- src/components/global/SubmitFormButton.vue
+++ src/components/global/SubmitFormButton.vue
@@ -1,26 +1,19 @@
 <script setup>
-import { defineProps, toRefs } from 'vue'
+import { defineProps, toRefs } from "vue";
 
 const props = defineProps({
-    btnText: String,
-    className: String,
-    id: String,
-    btnType: String
-})
+  btnText: String,
+  className: String,
+  iconClass: String,
+  id: String,
+  btnType: String,
+});
 
-const { btnText, className, id, btnType } = toRefs(props)
-
-
-
+const { btnText, className, iconClass, id, btnType } = toRefs(props);
 </script>
 
 <template>
-    <button 
-        :id="id"
-        :class="className" 
-        :type="btnType"
-        @click="$emit('submit', true)"
-    >
-        {{ btnText }}
-    </button>
+  <button :id="id" :class="className" :type="btnType" @click="$emit('submit', true)">
+    <i class="d-none" :class="iconClass"></i>{{ btnText }}
+  </button>
 </template>
