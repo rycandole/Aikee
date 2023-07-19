821639f7a74682e83cf1d6687a80cee25cf9c194
SLEC ITWebDev <aikee.slec@gmail.com>
2023-07-19 15:31:23 +0800

    Timer with SWAL

--- src/components/global/TimerAlertBox.vue
+++ src/components/global/TimerAlertBox.vue
@@ -1,39 +1,133 @@
-<script>
+<script setup>
+import axios from "axios";
 import { ref } from "@vue/reactivity";
-import moment from 'moment'
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { defineProps, toRefs } from "vue";
+import { useUSIndividualSched } from "@/store/us-individual-sched";
+import { useUSIndividualDetails } from "@/store/us-individual-details";
+import moment from "moment";
+import Swal from "@/sweetalert2";
 
-export default {
-  setup() {
-    const days = ref(0);
-    const hours = ref(0);
-    const minutes = ref(0);
-    const seconds = ref(0);
-    // const launchYear = new Date().getFullYear()
-    // const launchMonth = new Date().getMonth() + 1
-    // const launchDay = new Date().getDate() + 1
-    // const launchDate = new Date(launchYear+"-"+launchMonth+"-"+launchDay);
-    const launchMinute = moment(new Date().getTime()).add(30, 'minutes')
+const router = useRouter();
+const US_IndividualSched = useUSIndividualSched();
+const USIndividualDetails = useUSIndividualDetails();
 
-    setInterval(() => {
-      const timeNow = moment(new Date().getTime())
-    //   const currDate = new Date();
-      const launchTime = launchMinute - timeNow;
+const props = defineProps({
+  divClassName: String,
+});
 
-      seconds.value = parseInt(launchTime / 1000);
-      minutes.value = parseInt(seconds.value / 60);
-      hours.value = parseInt(minutes.value / 60);
-      days.value = parseInt(hours.value / 24);
-    }, 1000);
+const { divClassName } = toRefs(props);
 
-    return { days, hours, minutes, seconds };
-  },
+// const inputComputed = computed({
+//   get: () => input.value,
+//   set: (val) => emit("update:input", val),
+// });
+
+const schedule = JSON.parse(localStorage.getItem("us-individual-sched"));
+
+let sched_timer = schedule.timer;
+
+const minutes = ref(0);
+const seconds = ref(0);
+const launchMinute = new Date(sched_timer);
+
+onMounted(() => {
+  registrationTimer();
+});
+
+const registrationTimer = () => {
+  let timer = setInterval(() => {
+    const timeNow = moment(new Date().getTime());
+    const launchTime = launchMinute - timeNow;
+
+    if (launchTime <= 0) {
+      clearInterval(timer);
+
+      Swal.fire({
+        icon: "warning",
+        title: "Your time is up!",
+        text: "Do you still want to continue? Add 5 minutes.",
+        showDenyButton: true,
+        confirmButtonText: "Yes",
+        denyButtonText: "No",
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          const requestPAYLOAD = {
+            branch: useUSIndividualSched().branch,
+            country: useUSIndividualSched().country,
+            date: useUSIndividualSched().date,
+            time: useUSIndividualSched().time,
+            timer: moment(new Date().getTime()).add(5, "minutes"),
+          };
+
+          let res = JSON.stringify(requestPAYLOAD);
+
+          US_IndividualSched.setUSIndividualSched(res);
+
+          // location.reload();
+
+          Swal.fire({
+            icon: "success",
+            title: "You have extra 5 minutes",
+            text: "Please finish the registration form",
+            showConfirmButton: false,
+            timer: 2500,
+          });
+        } else if (result.isDenied) {
+          Swal.fire(
+            "Your registration is cancelled",
+            "The slot and details you input is gone",
+            "info"
+          );
+          moveBackSlot();
+        }
+      });
+    }
+
+    seconds.value = parseInt(launchTime / 1000);
+    minutes.value = parseInt(seconds.value / 60);
+  }, 1000);
+
+  return { minutes, seconds };
+};
+
+const moveBackSlot = async () => {
+  const jsonDATA = {
+    branch: useUSIndividualSched().branch,
+    country: useUSIndividualSched().country,
+    date: useUSIndividualSched().date,
+    time: useUSIndividualSched().time,
+    timer: useUSIndividualSched().timer,
+  };
+
+  let remove_slot = await axios.post("remove_slot/", jsonDATA);
+
+  if (remove_slot.data.status_code === 200) {
+    US_IndividualSched.clearUSIndividualSched();
+    USIndividualDetails.clearUSIndividualDetails();
+    router.push("/individual/us/schedule");
+  }
 };
 </script>
 <template>
-    <div class="alert alert-dismissible w-75" :class="minutes % 60 >= 10 ? 'alert-info' : 'alert-warning' ">
-        <h3><i class="icon fas" :class="minutes % 60 >= 10 ? 'fa-info' : 'fa-exclamation-triangle' "></i> <span class="ml-3">You only have {{ minutes % 60 }} {{ minutes % 60 > 1 ? "minutes" : "minute" }} and {{ seconds % 60 }} {{ seconds % 60 > 1 ? "seconds" : "second" }}</span></h3>
-    </div>
+  <div
+    class="alert alert-dismissible"
+    :class="[divClassName, minutes % 60 >= 1 ? 'alert-info' : 'alert-danger']"
+  >
+    <span class="fs-1"
+      >You only have <strong>{{ minutes % 60 }}</strong>
+      {{ minutes % 60 > 1 ? "minutes" : "minute" }} and
+      <strong>{{ seconds % 60 }}</strong>
+      {{ seconds % 60 > 1 ? "seconds" : "second" }} to finish registration.
+    </span>
+    <!-- <input type="text" :value="minutes % 60" :name="MinuteName" />
+    <input type="text" :value="seconds % 60" :name="SecondName" /> -->
+  </div>
 </template>
 <style lang="scss">
-
+.alert-dismissible {
+  box-shadow: -3px 3px 10px 0 #b1b3b3;
+}
 </style>
