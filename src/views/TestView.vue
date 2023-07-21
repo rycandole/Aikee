0b1788c764a4fcdf44d6516b39b1e5b38c9caffe
SLEC ITWebDev <aikee.slec@gmail.com>
2023-07-21 15:14:57 +0800

    Di pa ayos

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,47 +1,98 @@
-<script>
-import { ref } from "@vue/reactivity";
+<script setup>
+import { ref } from "vue";
+import { Form, Field, ErrorMessage } from "vee-validate";
+import * as yup from "yup";
 
-export default {
-  setup() {
-    const days = ref(0);
-    const hours = ref(0);
-    const minutes = ref(0);
-    const seconds = ref(0);
-    const launchDate = new Date("2023-07-18");
+let phone_1 = ref(null);
+let phone_2 = ref(null);
+let phone_3 = ref(null);
 
-    setInterval(() => {
-      const currDate = new Date();
-      const launchTime = launchDate - currDate;
-
-      seconds.value = parseInt(launchTime / 1000);
-      minutes.value = parseInt(seconds.value / 60);
-      hours.value = parseInt(minutes.value / 60);
-      days.value = parseInt(hours.value / 24);
-    }, 1000);
-
-    return { days, hours, minutes, seconds };
-  },
+const handleDetails = () => {
+  alert(phone_1.value + " | " + phone_2.value + " | " + phone_3.value);
 };
+
+// const schema = yup.object().shape({
+//   memberType: yup.string().required(),
+//   phone1: yup.string().required(),
+//   phone2: yup
+//     .string()
+//     .trim()
+//     .when("memberType", {
+//       is: "individual",
+//       then: yup.string().required("Must enter phone number"),
+//     }),
+// });
+
+const schema = yup.object().shape(
+  {
+    phone1: yup.string().when(["phone2", "phone3"], {
+      is: (phone2, phone3) => !phone2 && !phone3,
+      then: yup.string().required("Please enter one of the three fields"),
+    }),
+    phone2: yup.string().when(["phone1", "phone3"], {
+      is: (phone1, phone3) => !phone1 && !phone3,
+      then: yup.string().required("Please enter one of the three fields"),
+    }),
+    phone3: yup.string().when(["phone1", "phone2"], {
+      is: (phone1, phone2) => !phone1 && !phone2,
+      then: yup.string().required("Please enter one of the three fields"),
+    }),
+  },
+  [
+    ["phone1", "phone2"],
+    ["phone1", "phone3"],
+    ["phone2", "phone3"],
+  ]
+);
+
+schema.validate({ phone1: "", phone2: "", phone3: "" }).catch(function (err) {
+  console.log(err.errors[0]);
+});
+// const schema = yup.object.shape({
+//   phone1: yup.string().required(),
+// });
 </script>
 <template>
-  <div class="time_box">
-    <p>{{ days }} day</p>
-    <p>{{ hours % 24 }} hrs</p>
-    <p>{{ minutes % 60 }} min</p>
-    <p>{{ seconds % 60 }} sec</p>
+  <div>
+    <br />
+    <Form
+      @submit="handleDetails"
+      :validation-schema="schema"
+      class="col-lg-9 col-md-12 col-sm-12 mb-3"
+    >
+      <Field
+        type="text"
+        placeholder="Phone 1"
+        id="my_phone1"
+        name="phone1"
+        v-model="phone_1"
+        width="100px"
+      /><br />
+      <ErrorMessage name="phone1" class="text-danger" /><br />
+
+      <Field
+        type="text"
+        placeholder="Phone 2"
+        id="my_phone2"
+        name="phone2"
+        v-model="phone_2"
+        width="100px"
+      /><br />
+      <ErrorMessage name="phone2" class="text-danger" /><br />
+
+      <Field
+        type="text"
+        placeholder="Phone 3"
+        name="phone3"
+        v-model="phone_3"
+        width="100px"
+      /><br />
+      <ErrorMessage name="phone3" class="text-danger" /><br />
+
+      <br />
+      <button id="memberType" value="individual" type="submit">
+        <strong>SUBMIT</strong>
+      </button>
+    </Form>
   </div>
 </template>
-<style lang="scss">
-.time_box p {
-  padding: 10px;
-  background: grey;
-  color: #fff;
-  margin: 10px;
-  font-size: 20pt;
-  float: left;
-}
-.time_box {
-  margin-left: 35%;
-  margin-top: 20%;
-}
-</style>
